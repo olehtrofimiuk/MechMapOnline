3a9ee664b8de1a3fda0c3fd227439164bc460966
JetsunHD <olehtrofimiuk2@gmail.com>
2025-06-05 23:38:19 +0300

    Online server fix

--- server/main.py
+++ server/main.py
@@ -198,7 +198,12 @@ app = FastAPI(lifespan=lifespan)
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # React dev server
+    allow_origins=[
+        "http://localhost:3000", 
+        "http://127.0.0.1:3000",
+        "http://mechmaponline.fun:3000",
+        "https://mechmaponline.fun:3000"
+    ],  # React dev server + production domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
